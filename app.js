4cfc8e4ee769ac397492f75b81b294251da2b801
Saka Ricky <sakaricky91@gmail.com>
2022-03-28 09:51:59 +0100

    feat: created header and landing page

---
+++ app.js
@@ -0,0 +1,25 @@
+const toggleButton = document.querySelector('.toggle__menu');
+const header = document.querySelector('.header');
+const mobileNav = document.querySelector('.mobile__nav');
+const overlay = document.querySelector('.overlay');
+
+toggleButton.addEventListener('click', () => {
+    header.classList.toggle('open');
+    if (header.classList.contains('open')) {
+        overlay.style.opacity= "0.4"
+        overlay.style.visibility= 'visible';
+        mobileNav.style.left = "50%"
+
+    } else {
+        mobileNav.style.left = "150%"
+        overlay.style.opacity= "0";
+        overlay.style.visibility= "hidden"
+    }
+})
+
+overlay.addEventListener('click', () => {
+    header.classList.remove('open');
+    overlay.style.opacity= "0"
+    overlay.style.visibility= 'hidden';
+    mobileNav.style.left = "150%"
+})
